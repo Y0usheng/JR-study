486fad3433a948ff33f5b0fa6206e8f56ef92518
Deyi <z5464292@ad.unsw.edu.au>
2026-01-20 13:41:10 +1100

    "Setup CI/CD pipeline"

--- cal/income-tax-cal/vite.config.js
+++ cal/income-tax-cal/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/JR-study/', // 这里必须填你的【仓库名】，前后都要有斜杠
+  test: {
+    environment: 'jsdom', // 单元测试环境配置
+    globals: true,
+  }
 })
